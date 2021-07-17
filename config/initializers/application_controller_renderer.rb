6d9c66726c1eb34a512e9384d993827ea73949f0
springkyu <seika.k.r0610@gmail.com>
2021-07-17 14:08:19 +0000

    [UPDATE]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '510ffd8e92b91fdbc24cc5e8834f096741d135975ce25d1a6682a61fe0694438dc82b5e514cc0a602c054e9408bc8dd6d8efde62513cf2511d2aab96d45634f3'
